20ef3041798a8b91bd399120928c59c00c856ea9
lauraacristian <lauraa.cristian97@gmail.com>
2025-08-26 16:24:48 +0100

    hosting

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,20 @@
 const { Pool } = require("pg");
 
-require("dotenv").config();
+const ENV = process.env.NODE_ENV || "development";
 
-const pool = new Pool();
+require("dotenv").config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
 
-module.exports = pool;
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
+}
+
+const config = {};
+
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+module.exports = new Pool(config);
